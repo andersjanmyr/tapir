bd74f705acedb96bc3fa3895fb7d7b39cda3c1cf
Anders Janmyr <anders@janmyr.com>
2014-05-09 12:38:10 +0200

    fixed copy paste error

--- server.js
+++ server.js
@@ -3,9 +3,10 @@
 require('coffee-script/register');
 var httpServer = require('./lib/http-server');
 
-process.on 'uncaughtException', (error) ->
-    console.log error.stack
-    process.exit 1
+process.on('uncaughtException', function(error) {
+    console.log(error.stack);
+    process.exitr(1);
+});
 
 var port = process.env.PORT || 3000;
 httpServer.listen(port, function() {
